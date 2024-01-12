e2c8d4b57e9695948fa79907f23b0ab700db2661
Malena <malenavillaabrille@gmail.com>
2024-01-12 14:52:48 -0300

    modificacion login
    css y html de login, falta login con otras cuentas

---
+++ src/middlewares/sitioMantenimiento.js
@@ -0,0 +1,10 @@
+let enMantenimiento = true;
+
+function sitioMantenimiento(req, res, next){
+    if (enMantenimiento == true){
+        res.render("sitioMantenimiento.ejs")
+    }
+    next()
+}
+
+module.exports = sitioMantenimiento;
