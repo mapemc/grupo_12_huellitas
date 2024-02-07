2622b63beb3e7c33989228788f4239db4ad783fc
LinaCronopia <linabarauskas@hotmail.com>
2024-02-07 12:48:00 -0300

    formularios de servicios, rutas y controllers

--- src/controllers/servicesController.js
+++ src/controllers/servicesController.js
@@ -3,7 +3,17 @@ const path = require('path');
 //const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 
 const servicesController = {
+    guarderia: (req, res) =>{
+        res.render("formGuarderia");
+    },
 
+    peluqueria: (req, res) =>{
+        res.render("formPeluqueria");
+    },
+
+    veterinarios: (req, res) =>{
+        res.render("formVeterinarios");
+    },
 }
 
 module.exports = servicesController;
--- src/routes/servicesRouter.js
+++ src/routes/servicesRouter.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const servicesController = require("../controllers/servicesController.js");
 
-router.get("/services", servicesController.vet)
+router.get("/services/guarderia", servicesController.guarderia)
+router.get("/services/peluqueria", servicesController.peluqueria)
+router.get("/services/veterinarios", servicesController.veterinarios)
 /* 
 router.get("/create", productController.create);
 router.post("/create", productController.processCreate); */
