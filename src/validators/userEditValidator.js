91bd773bcc958dd04df3545c7f95729855bfd0e9
Malena <malenavillaabrille@gmail.com>
2024-02-22 01:28:32 -0300

    agregado middleware editValidator
    se agrega una base de lo que tendria que verificarse en actualizar perfil,sobretodo el cambio de contraseñas

---
+++ src/validators/userEditValidator.js
@@ -0,0 +1,20 @@
+const {check} = require('express-validator');
+
+const validationsEditProfile = [
+    check('password')
+        .notEmpty()
+        .withMessage('La contraseña es incorrecta'),
+    check('password')
+        .isStrongPassword({ minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
+        .withMessage('El campo contraseña debe contener al menos 6 caracteres: 1 minúscula, 1 mayúscula, 1 número, 1 símbolo')
+        .bail(),
+    check('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Las contraseñas no coinciden');
+            }
+            return true;
+        })
+];
+
+module.exports = validationsEditProfile
