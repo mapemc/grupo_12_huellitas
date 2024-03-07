6bc6f1dca3b47ad697ead5af590724c0efb1df48
AxelRoseSan <148290089+AxelRoseSan@users.noreply.github.com>
2024-03-07 17:53:41 -0500

    agregé gir ignore

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
     username: "root",
-    password: null,
-    database: "Huellitas",
+    password: "2404",
+    database: "huellitas",
     host: "127.0.0.1",
     dialect: "mysql",
   },
